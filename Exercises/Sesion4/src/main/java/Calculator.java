5b0e4a50c5cf3ef130e13b70001fb7742e0a8d0d
luis.pineda <luis.pineda@globant.com>
2022-04-05 16:19:13 -0500

    Advanced Calculator

--- Exercises/Sesion4/src/main/java/Calculator.java
+++ Exercises/Sesion4/src/main/java/Calculator.java
@@ -4,23 +4,37 @@ public class Calculator {
 
     public static void menu(){
         System.out.println("*********************");
-        System.out.println("Enter a digit according the following menu");
+        System.out.println("Enter a di8git according the following menu");
         System.out.println("1 - Sum");
         System.out.println("2 - Subtract");
+        System.out.println("3 - Multiply");
+        System.out.println("4 - Divide");
         System.out.println("Q - Quit App");
         System.out.println("*********************");
     }
 
-    public static int sumNumbers(int n1, int n2){
-        int result;
+    public static double sumNumbers(double n1, double n2){
+        double result;
         return result = n1 + n2;
     }
 
-    public static int sumSubtract(int n1, int n2){
-        int result;
+    public static double subtractNumbers(double n1, double n2){
+        double result;
         return result = n1 - n2;
     }
 
+    public static Double multiplyNumbers(double n1, double n2){
+        double result;
+        return result = n1 * n2;
+    }
+
+    public static Double divideNumbers(double n1, double n2){
+        double result;
+        if(n2==0){
+           return result = -1;
+        }
+        return result = n1 / n2;
+    }
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
@@ -28,7 +42,7 @@ public class Calculator {
         while(true){
             menu();
             ans = lector.nextLine();
-            if(ans.toUpperCase().equals("Q")){
+            if(ans.equalsIgnoreCase("Q")){
                 break;
             }
             if(ans.equals("1")){
@@ -43,7 +57,21 @@ public class Calculator {
                 int n1 = Integer.parseInt(lector.nextLine());
                 System.out.println("Enter the second number to subtract");
                 int n2 = Integer.parseInt(lector.nextLine());
-                System.out.println("The result is: " + sumSubtract(n1,n2));
+                System.out.println("The result is: " + subtractNumbers(n1,n2));
+            }
+            if(ans.equals("3")){
+                System.out.println("Enter the first number to multiply");
+                int n1 = Integer.parseInt(lector.nextLine());
+                System.out.println("Enter the second number to multiply");
+                int n2 = Integer.parseInt(lector.nextLine());
+                System.out.println("The result is: " + multiplyNumbers(n1,n2));
+            }
+            if(ans.equals("4")){
+                System.out.println("Enter the first number to divide");
+                int n1 = Integer.parseInt(lector.nextLine());
+                System.out.println("Enter the second number to divide");
+                int n2 = Integer.parseInt(lector.nextLine());
+                System.out.println("The result is: " + divideNumbers(n1,n2));
             }
         }
     }
